903c67442172ca3aa709642e969c41fc655f76d9
marshuang80 <marshuang80@gmail.com>
2020-06-28 11:21:32 -0700

    [NEW] resize and rescale function for dataloader

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import torch
 import torch.optim as optim
+import cv2
 
 
 class TwoCropTransform:
@@ -93,3 +94,48 @@ def save_model(model, optimizer, opt, epoch, save_file):
     }
     torch.save(state, save_file)
     del state
+
+
+def resize_img(img, scale):
+    """
+    args:
+        img - image as numpy array (cv2)
+        scale - desired output image-size as scale x scale
+    return:
+        image resized to scale x scale with shortest dimension 0-padded
+    """
+    size = img.shape
+    max_dim = max(size)
+    max_ind = size.index(max_dim)
+
+    #Resizing
+    if max_ind == 0:
+        #image is heigher
+        wpercent = (scale / float(size[0]))
+        hsize = int((float(size[1]) * float(wpercent)))
+        desireable_size = (scale, hsize)
+    else:
+        #image is wider
+        hpercent = (scale / float(size[1]))
+        wsize = int((float(size[0]) * float(hpercent)))
+        desireable_size = (wsize, scale)
+    resized_img = cv2.resize(img, desireable_size[::-1]) #this flips the desireable_size vector
+
+    #Padding
+    if max_ind == 0:
+        # height fixed at scale, pad the width
+        pad_size = scale - resized_img.shape[1]
+        left = int(np.floor(pad_size/2))
+        right = int(np.ceil(pad_size/2))
+        top = int(0)
+        bottom = int(0)
+    else:
+        # width fixed at scale, pad the height
+        pad_size = scale - resized_img.shape[0]
+        top = int(np.floor(pad_size/2))
+        bottom = int(np.ceil(pad_size/2))
+        left = int(0)
+        right = int(0)
+    resized_img = np.pad(resized_img,[(top, bottom), (left, right)], 'constant', constant_values=0)
+
+    return resized_img
