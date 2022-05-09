48140375921682b905915ec5c724bca5dd65c40e
yuanlonghui <1690957564@qq.com>
2022-05-09 21:40:40 +0800

    fix a bug, which has the little probability of producing nan in the loss

--- losses.py
+++ losses.py
@@ -86,7 +86,7 @@ class SupConLoss(nn.Module):
 
         # compute log_prob
         exp_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True) + 1e-6) # prevent computing log(0), which will produce Nan in the loss
 
         # compute mean of log-likelihood over positive
         mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
