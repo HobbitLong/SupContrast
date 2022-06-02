b889d60a6fe3c46cb4ee625e7dae1df7eb19f4ec
shovelingpig <flux1126@gmail.com>
2022-06-02 00:04:48 +0000

    Deactivate transforms for image augmentation

--- main_supcon.py
+++ main_supcon.py
@@ -144,12 +144,12 @@ def set_loader(opt):
     normalize = transforms.Normalize(mean=mean, std=std)
 
     train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomApply([
-            transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
-        ], p=0.8),
-        transforms.RandomGrayscale(p=0.2),
+        # transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
+        # transforms.RandomHorizontalFlip(),
+        # transforms.RandomApply([
+        #     transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+        # ], p=0.8),
+        # transforms.RandomGrayscale(p=0.2),
         transforms.ToTensor(),
         normalize,
     ])
