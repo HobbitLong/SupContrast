b5ebbeef857a7979a8fee90213672c67f2a76bc8
shovelingpig <flux1126@gmail.com>
2022-06-02 09:53:11 +0000

    Change optimizer from SGD to Adam

--- util.py
+++ util.py
@@ -76,9 +76,8 @@ def warmup_learning_rate(args, epoch, batch_id, total_batches, optimizer):
 
 
 def set_optimizer(opt, model):
-    optimizer = optim.SGD(model.parameters(),
+    optimizer = optim.Adam(model.parameters(),
                           lr=opt.learning_rate,
-                          momentum=opt.momentum,
                           weight_decay=opt.weight_decay)
     return optimizer
 
